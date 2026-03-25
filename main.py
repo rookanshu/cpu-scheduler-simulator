137e47f3bb61830f663e653adfa429b6010ea811
kex <himanshubhandari937@gmail.com>
2026-03-25 23:30:44 +0530

    Implemented SJF non-preemptive scheduling

--- main.py
+++ main.py
@@ -38,3 +38,49 @@ avg_tat = sum(r[5] for r in result) / n
 
 print(f"\nAverage Waiting Time: {avg_wt:.3f}")
 print(f"Average Turnaround Time: {avg_tat:.3f}")
+
+time = 0
+completed = 0
+n = len(processes)
+
+visited = [False] * n
+result = []
+
+while completed < n:
+    idx = -1
+    min_bt = float('inf')
+
+    for i in range(n):
+        if processes[i][1] <= time and not visited[i]:
+            if processes[i][2] < min_bt:
+                min_bt = processes[i][2]
+                idx = i
+
+    if idx == -1:
+        time += 1
+        continue
+
+    pid, at, bt = processes[idx]
+
+    start = time
+    completion = time + bt
+
+    tat = completion - at
+    wt = tat - bt
+
+    result.append([pid, at, bt, start, completion, tat, wt])
+
+    time = completion
+    visited[idx] = True
+    completed += 1
+
+print("\nSJF Scheduling")
+print("PID AT BT ST CT TAT WT")
+for r in result:
+    print(*r)
+
+avg_wt = sum(r[6] for r in result) / n
+avg_tat = sum(r[5] for r in result) / n
+
+print(f"\nAverage Waiting Time: {avg_wt:.3f}")
+print(f"Average Turnaround Time: {avg_tat:.3f}")
