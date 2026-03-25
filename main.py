f59ecbc5dfc918982dd6a2053282127b1aaa6a8e
kex <himanshubhandari937@gmail.com>
2026-03-25 22:42:17 +0530

    Added process input system

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("Project Started")
+n = int(input("Enter number of processes: "))
+processes = []
 
+for i in range(n):
+	pid = f"P{i+1}"
+	at = int(input(f"Arrival Time for {pid}: "))
+	bt = int(input(f"Burst Time for {pid}: "))
+	
+	processes.append([pid, at, bt])
+
+print("\nProcesses entered:")
+for p in processes:
+	print(p)
